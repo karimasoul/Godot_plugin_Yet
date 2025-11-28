421b6da6c2b3355a2a196f7afe404aabf9d8ebd1
karimasoul <ouadkarima@outlook.com>
2025-11-28 01:39:47 +0100

    drag and drop pas finie

--- addons/yetanotherplugins/dock.gd
+++ addons/yetanotherplugins/dock.gd
@@ -8,9 +8,15 @@ var editor_interface: EditorInterface
 @onready var hide_window: CheckBox = $VBoxContainer2/CheckBox
 @onready var hide_resolution: CheckBox = $VBoxContainer3/CheckBox
 @onready var hide_vertical: CheckBox = $VBoxContainer4/CheckBox
+@onready var drop_area: Panel = $VBoxContainer5/Panel #pas finie
+var selected_scene: PackedScene = null
+
 
 
 func _ready():
+	# Permet le drag & drop pas finie
+	drop_area.gui_input.connect(_on_drop_area_input)
+	#
 	hide_volume.button_pressed = true
 	hide_window.button_pressed = true
 	hide_resolution.button_pressed = true
@@ -36,7 +42,18 @@ func _ready():
 		print("Bouton du dock introuvable.")
 
 
-
+#pas finie
+func _on_drop_area_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		# juste un print pour test
+		print("Click sur la drop area")
+		
+	if event is InputEventMouse:
+		var data = event.get_drag_data() if event.has_drag_data() else null
+		if data and data is PackedScene:
+			selected_scene = data
+			print("Scene sélectionnée:", selected_scene)
+#
 func _on_toggled_volume(checked: bool):
 	if not editor_interface:
 		return 
